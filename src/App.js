1e881db383271ed3f34c9e745f7acd4c74374018
Idrees Khan <khanidrees1234@gmail.com>
2021-05-13 15:02:50 +0530

    added Tasks component and used useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { useState } from 'react'
+
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+      id : 1,
+      text : 'task1'
+    },
+    {
+      id : 2,
+      text : 'task2'
+    },
+    {
+    id : 3,
+    text : 'task3'
+    }
+
+  ]);
   return (
-    <div className="App">
-      <Header/>
+    <div className="container">
+      <Header title = "Task Tracker" />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
   return (
-    <header>
-      <h1>Task Tracker</h1> 
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button text="Add" color="green"/>
     </header>
   )
+}
+
+Header.propTypes = {
+  title : PropTypes.string,
 }
 
 export default Header
