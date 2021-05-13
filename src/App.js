d47aca764b70c80ee3396d8b1c2ee070c4e647b2
Idrees Khan <khanidrees1234@gmail.com>
2021-05-13 16:21:26 +0530

    toggle task reminder

--- src/App.js
+++ src/App.js
@@ -8,22 +8,43 @@ function App() {
   const [tasks, setTasks] = useState([
     {
       id : 1,
-      text : 'task1'
+      text : 'task1',
+      day : 'date1',
+      reminder : false
     },
     {
       id : 2,
-      text : 'task2'
+      text : 'task2',
+      day : 'date2',
+      reminder : false
     },
     {
     id : 3,
-    text : 'task3'
+    text : 'task3',
+    day : 'date3',
+    reminder : false
     }
 
   ]);
+  //Delete Task
+  const deleteTask =(id)=>{
+    setTasks(tasks.filter((task)=>task.id!==id))
+  }
+  //Toggle Reminder
+  const toggleReminder=(id)=>{
+    setTasks(tasks.map((task)=>(task.id===id ?
+      {...task, reminder: !task.reminder} : 
+      task))
+    )
+  }
   return (
     <div className="container">
       <Header title = "Task Tracker" />
-      <Tasks tasks={tasks} />
+      { tasks.length> 0 ? (
+          <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+          ) : (
+          'No Tasks to Show'
+          ) }
     </div>
   );
 }
